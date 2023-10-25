a9ea71100f417e551fd8f3ae32c040dfd9b8010c
andresant-ana <rm551575@fiap.com.br>
2023-10-24 23:34:00 -0300

    Commit de finalização dos cards.

--- src/app/produtos/[slug]/page.jsx
+++ src/app/produtos/[slug]/page.jsx
@@ -10,18 +10,34 @@ export default function ProdutoSlug({ params }) {
       {
         params.slug == 'camisa' ?
         <Card
-          titulo='Camiseta daora'
+          titulo='CAMISA'
           fonte='https://fideli.vtexassets.com/arquivos/ids/158668/R011607986_916_1-CAMISA-SOCIAL-COLCLASSICO-S-PM-REGULAR.jpg?v=637667858346870000'
           alt='Camisa'
         />:
         params.slug == 'calca' ?
-        <Card titulo='Calça daora'/>:
+        <Card
+          titulo='CALÇA'
+          fonte=''
+          alt='Calça'
+        />:
         params.slug == 'sapato' ?
-        <Card titulo='Sapato daora'/>:
+        <Card
+          titulo='SAPATO'
+          fonte=''
+          alt='Sapato'
+        />:
         params.slug == 'bone' ?
-        <Card titulo='Boné daora'/>:
+        <Card
+          titulo='BONÉ'
+          fonte=''
+          alt='Boné'
+        />:
         params.slug == 'oculos' ?
-        <Card titulo='Óculos daora'/>:
+        <Card
+          titulo='ÓCULOS' 
+          fonte=''
+          alt='Óculos'
+        />:
         <Image className="mx-auto w-75"
           src="https://img.freepik.com/free-vector/error-404-concept-illustration_114360-1811.jpg?w=1380&t=st=1698188171~exp=1698188771~hmac=68ba6e5cd75c5438cb91c9e0aecdaf04e5c8c64258b9013f2faff3f1b72d90e0"
           alt="404"
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,8 +7,8 @@ export default function Card({titulo, fonte, alt, altura, largura}) {
             tit: tit ? tit : 'Titulo Padrão',
             fonte: fonte ? fonte : "https://img.freepik.com/free-vector/error-404-concept-illustration_114360-1811.jpg?w=1380&t=st=1698188171~exp=1698188771~hmac=68ba6e5cd75c5438cb91c9e0aecdaf04e5c8c64258b9013f2faff3f1b72d90e0",
             alt: alt ? alt : 'Alt Padrão',
-            altura: altura ? altura : 100,
-            largura: largura ? largura : 100
+            altura: altura ? altura : 200,
+            largura: largura ? largura : 200
         }
     }
 
@@ -16,10 +16,10 @@ export default function Card({titulo, fonte, alt, altura, largura}) {
 
     return (
         <div className="w-auto h-auto my-0 mx-auto flex items-center justify-center mt-10">
-            <div className="flex items-center justify-center rounded-md w-1/3 h-96 bg-red-500">
-                <h1>{cp.tit}</h1>
-
+            <div className="flex-col items-center justify-center rounded-md w-1/3 h-96 bg-slate-300 shadow-lg shadow-inner">
+                <h1 className="font-extrabold text-4xl text-slate-800 text-center mt-2 mb-2">{cp.tit}</h1>
                 <Image
+                    className="mx-auto"
                     src={cp.fonte}
                     alt={cp.alt}
                     width={cp.largura}
